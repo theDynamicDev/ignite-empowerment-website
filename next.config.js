738bb8ebe8cc44439c38b09af91689877cf62e18
Omar <omar@theDynamic.dev>
2023-06-28 18:22:18 -0400

    almost done

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-          {
-            source: '/public/:path*',
-            destination: '/:path*',
-          },
-        ];
+  async rewrites() {
+    return [
+      {
+        source: '/public/:path*',
+        destination: '/:path*',
       },
+    ]
+  },
+  images: {
+    domains: ['cdn.sanity.io'],
+  },
 }
 
 module.exports = nextConfig
--- src/app/register/layout.jsx
+++ src/app/register/layout.jsx
@@ -1,19 +1,13 @@
-import React from 'react'
-import NavBar from '../components/NavBar'
-function layout({children}) {
+import React from "react";
+import NavBar from "../components/NavBar";
+function layout({ children }) {
   return (
-<html lang="en" className="  w-screen h-screen -z-10 bg-forms-background bg-opacity-10 bg-cover bg-no-repeat bg-fixed bg-center ">
-<body className="   flex flex-col justify-center items-center " >
-<header className={`z-20`}>
-
-<NavBar />
-  </header>
-  <div className=" flex ">
-    {children}
+    <div className="  w-screen h-screen -z-10 bg-forms-background bg-opacity-10 bg-cover bg-no-repeat bg-fixed bg-center ">
+      <div className="flex flex-col justify-center items-center ">
+        <div className=" flex ">{children}</div>
+      </div>
     </div>
-    </body>
-</html>
-  )
+  );
 }
 
-export default layout
+export default layout;
